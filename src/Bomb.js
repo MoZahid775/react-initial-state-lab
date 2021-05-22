55700c205547b105c6fc4bad0909b0e87676089d
Muhammad Zahid <muhammadali775@gmail.com>
2021-05-22 01:54:46 -0400

    commit tree eb8cb7d0a1ae665f94ce95481e0922a106ff9b1f

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,39 @@
 // your Bomb code here!
+import React, { Component } from 'react'
+
+export default class Bomb extends Component {
+    
+    
+    state={ 
+       secondsLeft : this.props.initialCount 
+
+    }
+
+   
+    
+    
+    
+    
+    
+    render() {
+      //let countDown = () => {
+           
+              
+              
+                 
+                 
+                 
+         // this.state.secondsLeft > 0 ? `${this.state.secondsLeft} seconds left before I go boom!` : `Boom!`
+          
+                 
+
+               //}
+        
+          return (
+              <div>
+              {this.state.secondsLeft > 0 ? `${this.state.secondsLeft} seconds left before I go boom!` : `Boom!`}
+              </div>
+        )
+    }
+}
+
